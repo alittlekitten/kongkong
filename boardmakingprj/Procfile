cb0257c14bbc7f961aa93431acdcc8d4ca4050ff
alittlekitten <alittlekitten>
2019-07-09 21:31:55 +0900

    first

---
+++ boardmakingprj/Procfile
@@ -0,0 +1,1 @@
+web: gunicorn boardmakingprj.wsgi --log-file -
---
+++ firstvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\dlscj\desktop\ttest\firstvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
